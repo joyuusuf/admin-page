67847a5343900c38e4933435dcfd9bfea412281f
Joy <yuusufjawadolamide67@gmail.com>
2024-10-13 09:21:24 +0100

    feature page with admin page file

--- app/page.tsx
+++ app/page.tsx
@@ -42,7 +42,10 @@ import {
   TableHead,
   TableHeader,
   TableRow,
-} from "@/components/ui/table"
+} from "@/components/ui/table";
+import { ModeToggle } from "@/components/ui/mode-toggle";
+
+import AdminSearch from "@/components/features/admin-search"
 
 export const description =
   "An application shell with a header and main content area. The header has a navbar, a search input and and a user nav dropdown. The user nav is toggled by a button with an avatar image."
@@ -141,16 +144,7 @@ export default function Dashboard() {
           </SheetContent>
         </Sheet>
         <div className="flex w-full items-center gap-4 md:ml-auto md:gap-2 lg:gap-4">
-          <form className="ml-auto flex-1 sm:flex-initial">
-            <div className="relative">
-              <Search className="absolute left-2.5 top-2.5 h-4 w-4 text-muted-foreground" />
-              <Input
-                type="search"
-                placeholder="Search products..."
-                className="pl-8 sm:w-[300px] md:w-[200px] lg:w-[300px]"
-              />
-            </div>
-          </form>
+         <AdminSearch />
           <DropdownMenu>
             <DropdownMenuTrigger asChild>
               <Button variant="secondary" size="icon" className="rounded-full">
@@ -159,6 +153,7 @@ export default function Dashboard() {
               </Button>
             </DropdownMenuTrigger>
             <DropdownMenuContent align="end">
+              <ModeToggle />
               <DropdownMenuLabel>My Account</DropdownMenuLabel>
               <DropdownMenuSeparator />
               <DropdownMenuItem>Settings</DropdownMenuItem>
---
+++ components/features/admin-search.tsx
@@ -0,0 +1,40 @@
+"use client";
+import { z } from "zod"
+import { useForm } from "react-hook-form";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { Input } from "../ui/input";
+import { Search } from "lucide-react";
+
+
+const formSchema = z.object({
+  search: z.string(), /* if there are other items in the search, it will be added hre. */
+})
+export default function AdminSearch() {
+  const form = useForm<z.infer<typeof formSchema>>({
+
+    resolver: zodResolver(formSchema),
+
+    defaultValues: {
+      search: "",
+
+    },
+  });
+
+  const onSubmit = (values: z.infer<typeof formSchema>) => {
+    console.log(values)
+  }
+
+  return (
+    <form className="ml-auto flex-1 sm:flex-initial">
+      <div className="relative">
+        <Search className="absolute left-2.5 top-2.5 h-4 w-4 text-muted-foreground" />
+        <Input
+          type="search"
+          placeholder="Search products..."
+          className="pl-8 sm:w-[300px] md:w-[200px] lg:w-[300px]"
+        />
+      </div>
+    </form>
+  )
+}
+
