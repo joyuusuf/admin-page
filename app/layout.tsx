8d2fbdd04053ab35b2512427ee8b0a83ec8b9d13
Joy <yuusufjawadolamide67@gmail.com>
2024-10-13 12:31:12 +0100

    dashboard page

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import localFont from "next/font/local";
 import "./globals.css";
 import { ThemeProvider } from "@/components/ui/theme-provider";
+import Header from "./features/header/page";
 
 const geistSans = localFont({
   src: "./fonts/GeistVF.woff",
@@ -29,6 +30,8 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
+
+        <Header />
         <ThemeProvider
             attribute="class"
             defaultTheme="system"
